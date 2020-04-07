8cf844eb60dbf4495af295ac7ccdb662c49f98d2
KaranbirSingh118 <50712392+KaranbirSingh118@users.noreply.github.com>
2020-04-07 02:54:37 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,28 @@
+import { songInfo } from '../../app/songInfo';
+
+export const MYCLASSES: songInfo[] = [
+    {sname: 'In the end',
+     artist: 'Linken park',
+     genre: 'Rap/Rock',
+     release: '2002',
+     picture: 'assets/images/1.jpg', 
+     },
+     {sname: 'Cold',
+     artist: 'Neffex',
+     genre: 'Electronic music',
+     release: '2018',
+     picture: 'assets/images/2.jpg', 
+     },
+     {sname: 'Memories',
+     artist: 'Maroon 5',
+     genre: 'Pop',
+     release: '2019',
+     picture: 'assets/images/3.jpg', 
+     },
+     {sname: 'Giants',
+     artist: 'True Damage',
+     genre: 'Hip-Hop/Rap',
+     release: '2019',
+     picture: 'assets/images/4.jpg', 
+     }     
+    ];
